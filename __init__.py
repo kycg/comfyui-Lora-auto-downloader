901612aa183a623e4064717926872829a24fc1e9
kycg <95092804+kycg@users.noreply.github.com>
2024-11-08 10:17:37 +0000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
-from .nodes.cai_download import Kw_Json_Lora_CivitAIDownloader
-from .nodes.Json_Lora_loader import Kw_JsonLoraLoader
+from .cai_download import Kw_Json_Lora_CivitAIDownloader
+from .Json_Lora_loader import Kw_JsonLoraLoader
+
 
 NODE_CLASS_MAPPINGS = { 
     "Kw_Json_Lora_CivitAIDownloader":Kw_Json_Lora_CivitAIDownloader,
